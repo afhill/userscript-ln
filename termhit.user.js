af4176c98f20800fbfcdd7630c90517c3f9fce7a
Andrea <andrea.hill@gmail.com>
2018-07-29 07:38:17 -0400

    Created termhit.user.js
    Change CSS for results page

---
+++ termhit.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         LAQL - ResultsList - TermHits
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  New landing page search form
+// @author       Andrea Hill
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js
+// @resource     customcss https://raw.githubusercontent.com/afhill/userscript-ln/master/styles.css?v=3
+// @resource     selectycss https://raw.githubusercontent.com/bressljr/searchform/master/selecty.css?v=1
+// @resource     juriscss https://raw.githubusercontent.com/bressljr/searchform/master/hummingbird-treeview.css?v=5
+// @match        https://advance.lexis.com/search*
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+
+
+})();
